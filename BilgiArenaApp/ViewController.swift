d0c68a590a33e86c023e34d9fa49a2530eb80b7e
Zemfira Zeynalli <109147342+zemfirazeynal@users.noreply.github.com>
2025-04-02 03:45:11 -0700

    Initial Commit

---
+++ BilgiArenaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BilgiArenaApp
+//
+//  Created by Zemfira Asadzade on 02.04.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
