f917846975c80231be624be8c316a6a3b9facd68
Zemfira Zeynalli <109147342+zemfirazeynal@users.noreply.github.com>
2025-05-08 01:56:58 +0400

    commit
    commit

--- BilgiArenaApp/Builder/OtpCodeBuilder.swift
+++ BilgiArenaApp/Builder/OtpCodeBuilder.swift
@@ -9,8 +9,13 @@ import Foundation
 import UIKit
 
 struct OtpCodeBuilder {
-    static func build(email: String, coordinator: SignupFlowCoordinatorProtocol) -> UIViewController {
-        let viewModel = OtpCodeViewModel(flowType: .signup, coordinator: coordinator)
+    static func build(email: String, coordinator: SignupFlowCoordinatorProtocol)
+        -> UIViewController
+    {
+        let viewModel = OtpCodeViewModel(
+            flowType: .signup,
+            coordinator: coordinator
+        )
         viewModel.email = email
         let controller = OtpCodeViewController(viewModel: viewModel)
         return controller
