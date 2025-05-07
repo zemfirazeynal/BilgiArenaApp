f4144aab3dfa613d7e03451d5f526cf91bc903f4
Zemfira Zeynalli <109147342+zemfirazeynal@users.noreply.github.com>
2025-05-08 01:39:30 +0400

    commit
    commit

--- BilgiArenaApp/Screens/Auth/Login/LoginViewModel.swift
+++ BilgiArenaApp/Screens/Auth/Login/LoginViewModel.swift
@@ -9,28 +9,30 @@ import Foundation
 
 protocol LoginViewModelProtocol {
     var email: String { get set }
-       var password: String { get set }
-       var onError: ((String) -> Void)? { get set }
-    
+    var password: String { get set }
+    var onError: ((String) -> Void)? { get set }
+
     var onStateChange: ((ViewState) -> Void)? { get set }
 
-    
     func forgotPasswordTapped()
     func loginTapped()
 }
 
 final class LoginViewModel: LoginViewModelProtocol {
     var coordinator: LoginCoordinatorProtocol?
-        private let loginManager: LoginManagerUseCase
+    private let loginManager: LoginManagerUseCase
 
-        var email: String = ""
-        var password: String = ""
-        
-        var onError: ((String) -> Void)?
-    
+    var email: String = ""
+    var password: String = ""
+
+    var onError: ((String) -> Void)?
+
     var onStateChange: ((ViewState) -> Void)?
-    
-    init(coordinator: LoginCoordinatorProtocol?, loginManager: LoginManagerUseCase = LoginManager()) {
+
+    init(
+        coordinator: LoginCoordinatorProtocol?,
+        loginManager: LoginManagerUseCase = LoginManager()
+    ) {
         self.coordinator = coordinator
         self.loginManager = loginManager
     }
@@ -38,34 +40,40 @@ final class LoginViewModel: LoginViewModelProtocol {
     func forgotPasswordTapped() {
         coordinator?.showResetPassword()
     }
-    
-    func loginTapped() { //+
-        // 1. Lokal validasiya
-        if let errorMessage = validateCredentials(email: email, password: password) {
-               onStateChange?(.error(message: errorMessage))
-               return
-           }
 
-            // 2. Loading vəziyyətini bildir
-            onStateChange?(.loading)
+    func loginTapped() {  //+
+        if let errorMessage = validateCredentials(
+            email: email,
+            password: password
+        ) {
+            onStateChange?(.error(message: errorMessage))
+            return
+        }
 
-            // 3. API çağırışı
-            loginManager.login(email: email, password: password) { [weak self] response, error in
-                DispatchQueue.main.async {
-                    guard let self else { return }
+        onStateChange?(.loading)
 
-                    if let jwt = response?.jwt {
-                        KeychainService.shared.save(key: "jwt_token", value: jwt)
-                        self.coordinator?.showMainApp()
-                    } else {
-                        self.onStateChange?(.error(message: "The email or password you entered is incorrect. Please try again."))
-                        
-                    }
+        loginManager.login(email: email, password: password) {
+            [weak self] response, error in
+            DispatchQueue.main.async {
+                guard let self else { return }
+
+                if let jwt = response?.jwt {
+                    KeychainService.shared.save(key: "jwt_token", value: jwt)
+                    self.coordinator?.showMainApp()
+                } else {
+                    self.onStateChange?(
+                        .error(
+                            message:
+                                "The email or password you entered is incorrect. Please try again."
+                        )
+                    )
                 }
             }
+        }
     }
-    
-    private func validateCredentials(email: String, password: String) -> String? {
+
+    private func validateCredentials(email: String, password: String) -> String?
+    {
         if email.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
             return "Email must not be empty. Please try again."
         }
@@ -74,7 +82,4 @@ final class LoginViewModel: LoginViewModelProtocol {
         }
         return nil
     }
-    
-    
 }
-
--- BilgiArenaApp/Screens/Auth/OtpCode/OtpCodeViewController.swift
+++ BilgiArenaApp/Screens/Auth/OtpCode/OtpCodeViewController.swift
@@ -11,7 +11,6 @@ class OtpCodeViewController: UIViewController {
 
     private let navigationHeader = CustomNavigationHeaderView()
 
-
     // MARK: - UI Elements
     private let titleLabel: UILabel = {
         let label = UILabel()
@@ -31,8 +30,10 @@ class OtpCodeViewController: UIViewController {
     }()
 
     private let otpCodeTextField = AuthTextField(
-        placeholder: "Your OTP Code", icon: "login_lock_icon", isSecure: true)
-
+        placeholder: "Your OTP Code",
+        icon: "login_lock_icon",
+        isSecure: true
+    )
 
     private let nextButton: UIButton = {
         let button = UIButton(type: .system)
@@ -66,15 +67,15 @@ class OtpCodeViewController: UIViewController {
     // MARK: - Init
 
     private var viewModel: OtpCodeViewModel
-        
+
     init(viewModel: OtpCodeViewModel) {
-            self.viewModel = viewModel
-            super.init(nibName: nil, bundle: nil)
-        }
-        
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -88,10 +89,10 @@ class OtpCodeViewController: UIViewController {
         configureForFlowType()
         bindViewModel()
     }
-    
+
     private func configureForFlowType() {
         switch viewModel.flowType {
-            
+
         case .signup:
             progressView.isHidden = false
             progressLabel.isHidden = false
@@ -106,18 +107,20 @@ class OtpCodeViewController: UIViewController {
         otpCodeTextField.focus()  // bu, klaviaturanı göstərir
     }
 
-
-
-        private func setupActions() {
-            nextButton.addTarget(self, action: #selector(didTapNext), for: .touchUpInside)
-        }
-    
+    private func setupActions() {
+        nextButton.addTarget(
+            self,
+            action: #selector(didTapNext),
+            for: .touchUpInside
+        )
+    }
 
     // MARK: - Layout
     private func setupLayout() {
 
         [
-            navigationHeader, titleLabel, otpCodeLabel, otpCodeTextField,progressLabel, progressView,
+            navigationHeader, titleLabel, otpCodeLabel, otpCodeTextField,
+            progressLabel, progressView,
             nextButton,
         ].forEach {
             view.addSubview($0)
@@ -125,87 +128,104 @@ class OtpCodeViewController: UIViewController {
 
         NSLayoutConstraint.activate([
             navigationHeader.topAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.topAnchor),
+                equalTo: view.safeAreaLayoutGuide.topAnchor
+            ),
             navigationHeader.leadingAnchor.constraint(
-                equalTo: view.leadingAnchor),
+                equalTo: view.leadingAnchor
+            ),
             navigationHeader.trailingAnchor.constraint(
-                equalTo: view.trailingAnchor),
+                equalTo: view.trailingAnchor
+            ),
             navigationHeader.heightAnchor.constraint(equalToConstant: 48),
 
             titleLabel.centerYAnchor.constraint(
-                equalTo: navigationHeader.centerYAnchor),
+                equalTo: navigationHeader.centerYAnchor
+            ),
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
 
             otpCodeLabel.topAnchor.constraint(
-                equalTo: titleLabel.bottomAnchor, constant: 40),
+                equalTo: titleLabel.bottomAnchor,
+                constant: 40
+            ),
             otpCodeLabel.leadingAnchor.constraint(
-                equalTo: view.leadingAnchor, constant: 24),
+                equalTo: view.leadingAnchor,
+                constant: 24
+            ),
 
             otpCodeTextField.topAnchor.constraint(
-                equalTo: otpCodeLabel.bottomAnchor, constant: 12),
+                equalTo: otpCodeLabel.bottomAnchor,
+                constant: 12
+            ),
             otpCodeTextField.leadingAnchor.constraint(
-                equalTo: view.leadingAnchor, constant: 24),
+                equalTo: view.leadingAnchor,
+                constant: 24
+            ),
             otpCodeTextField.trailingAnchor.constraint(
-                equalTo: view.trailingAnchor, constant: -24),
+                equalTo: view.trailingAnchor,
+                constant: -24
+            ),
             otpCodeTextField.heightAnchor.constraint(equalToConstant: 56),
 
-
             progressLabel.topAnchor.constraint(
-                equalTo: otpCodeTextField.bottomAnchor, constant: 12),
+                equalTo: otpCodeTextField.bottomAnchor,
+                constant: 12
+            ),
             progressLabel.trailingAnchor.constraint(
-                equalTo: otpCodeTextField.trailingAnchor),
+                equalTo: otpCodeTextField.trailingAnchor
+            ),
 
             progressView.topAnchor.constraint(
-                equalTo: progressLabel.bottomAnchor, constant: 8),
+                equalTo: progressLabel.bottomAnchor,
+                constant: 8
+            ),
             progressView.leadingAnchor.constraint(
-                equalTo: otpCodeTextField.leadingAnchor),
+                equalTo: otpCodeTextField.leadingAnchor
+            ),
             progressView.trailingAnchor.constraint(
-                equalTo: otpCodeTextField.trailingAnchor),
+                equalTo: otpCodeTextField.trailingAnchor
+            ),
             progressView.heightAnchor.constraint(equalToConstant: 4),
 
             nextButton.bottomAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+                equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                constant: -16
+            ),
             nextButton.leadingAnchor.constraint(
-                equalTo: otpCodeTextField.leadingAnchor),
+                equalTo: otpCodeTextField.leadingAnchor
+            ),
             nextButton.trailingAnchor.constraint(
-                equalTo: otpCodeTextField.trailingAnchor),
+                equalTo: otpCodeTextField.trailingAnchor
+            ),
             nextButton.heightAnchor.constraint(equalToConstant: 56),
 
         ])
-        
-        
+
     }
 
     private func bindViewModel() {
         viewModel.onStateChange = { [weak self] state in
-                    DispatchQueue.main.async {
-                        switch state {
-                        case .idle:
-                            break
-                        case .loading:
-                            break
-                        case .success(_):
-                             break
-                        case .error(let message):
-                            self?.present(Alert.showAlert(title: "Error", message: message), animated: true)
-                        }
-                    }
+            DispatchQueue.main.async {
+                switch state {
+                case .idle:
+                    break
+                case .loading:
+                    break
+                case .success(_):
+                    break
+                case .error(let message):
+                    self?.present(
+                        Alert.showAlert(title: "Error", message: message),
+                        animated: true
+                    )
                 }
+            }
+        }
     }
-    
+
     // MARK: - Action
     @objc private func didTapNext() {
         viewModel.otpCode = otpCodeTextField.text ?? ""
         viewModel.proceedIfValid()
     }
 
-
-}
-
-extension UIViewController {
-    func showAlert(title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(.init(title: "OK", style: .default))
-        present(alert, animated: true)
-    }
 }
