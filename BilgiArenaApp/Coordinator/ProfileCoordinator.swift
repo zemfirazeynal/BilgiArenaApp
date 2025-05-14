1d3c862715b4082cd55035a2dfec17444d25356f
Zemfira Zeynalli <109147342+zemfirazeynal@users.noreply.github.com>
2025-05-14 20:04:53 +0400

    commit
    commit

--- BilgiArenaApp/Coordinator/ProfileCoordinator.swift
+++ BilgiArenaApp/Coordinator/ProfileCoordinator.swift
@@ -12,27 +12,32 @@ protocol ProfileCoordinatorProtocol: AnyObject {
     func showSettings()
 }
 
-final class ProfileCoordinator: Coordinator {
+final class ProfileCoordinator: Coordinator, ProfileCoordinatorProtocol {
     var navigationController: UINavigationController
 
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
 
+    //    func start() {
+    //        let controller = ProfileViewController()
+    //        let tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "tab_profile"), selectedImage: nil)
+    //        tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+    //        controller.tabBarItem = tabBarItem
+    //        navigationController.setViewControllers([controller], animated: true)
+    //    }
+
     func start() {
         let controller = ProfileViewController()
-        let tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "tab_profile"), selectedImage: nil)
-        tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
-        controller.tabBarItem = tabBarItem
-        navigationController.setViewControllers([controller], animated: true)
+        controller.viewModel = .init(coordinator: self)
+        navigationController.setViewControllers([controller], animated: false)
     }
 
-     func showSettings() {
-         print("coordinator tapped")
-         let controller = SettingsViewController()
-         let nav = UINavigationController(rootViewController: controller)
-         nav.modalPresentationStyle = .fullScreen
-         navigationController.present(nav, animated: true)
+    func showSettings() {
+        print("coordinator tapped")
+        let controller = SettingsViewController()
+        let nav = UINavigationController(rootViewController: controller)
+        nav.modalPresentationStyle = .fullScreen
+        navigationController.present(nav, animated: true)
     }
 }
-
--- BilgiArenaApp/Coordinator/SearchCoordinator.swift
+++ BilgiArenaApp/Coordinator/SearchCoordinator.swift
@@ -12,29 +12,36 @@ protocol SearchCoordinatorProtocol: AnyObject {
     func showQuizDetail(for quiz: Quiz)
 }
 
-final class SearchCoordinator: SearchCoordinatorProtocol {
-    
+final class SearchCoordinator: Coordinator, SearchCoordinatorProtocol {
+
     private var quizDetailsCoordinator: QuizDetailsCoordinator?
 
     var navigationController: UINavigationController
-    
 
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
 
     
-    func start(){
+    //    func start(){
+    //        let controller = SearchViewController()
+    //        let tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "tab_search"), selectedImage: nil)
+    //        tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+    //        controller.tabBarItem = tabBarItem
+    //        navigationController.setViewControllers([controller], animated: true)
+    //        }
+
+    func start() {
         let controller = SearchViewController()
-        let tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "tab_search"), selectedImage: nil)
-        tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
-        controller.tabBarItem = tabBarItem
-        navigationController.setViewControllers([controller], animated: true)
-        }
-    
-    
+        controller.viewModel = .init(coordinator: self)
+        navigationController.setViewControllers([controller], animated: false)
+    }
+
     func showQuizDetail(for quiz: Quiz) {
-        let coordinator = QuizDetailsCoordinator(navigationController: navigationController, quiz: quiz)
+        let coordinator = QuizDetailsCoordinator(
+            navigationController: navigationController,
+            quiz: quiz
+        )
         quizDetailsCoordinator = coordinator
         coordinator.start()
     }
