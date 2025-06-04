d4eecff734f84a2016e662bc81b8f251942348f6
Zemfira Zeynalli <109147342+zemfirazeynal@users.noreply.github.com>
2025-06-04 16:38:49 +0400

    commit
    commit

---
+++ BilgiArenaApp/Network/Endpoint/Search/SearchEndpoint.swift
@@ -0,0 +1,23 @@
+//
+//  SearchEndpoint.swift
+//  BilgiArenaApp
+//
+//  Created by Zemfira Asadzade on 02.06.25.
+//
+
+
+import Foundation
+
+enum SearchEndpoint {
+    case allQuizzes(page: Int, size: Int)
+    
+    var path: String {
+        switch self {
+        case let .allQuizzes(page, size):
+                    return NetworkHelper.shared.configureURL(
+                        endpoint: "quiz/all-quiz?pageNumber=\(page)&pageSize=\(size)"
+                    )
+                }
+        }
+    
+}
---
+++ BilgiArenaApp/Network/Endpoint/Search/SearchManager.swift
@@ -0,0 +1,53 @@
+//
+//  SearchManagerUseCase.swift
+//  BilgiArenaApp
+//
+//  Created by Zemfira Asadzade on 02.06.25.
+//
+
+
+import Foundation
+
+protocol SearchManagerUseCase {
+    func allQuizzes(
+        page: Int,
+        size: Int,
+        completion: @escaping (Result<[PublicQuizItemresponseData], Error>) -> Void
+    )
+}
+
+final class SearchManager: SearchManagerUseCase {
+    private let manager = NetworkManager()
+    
+    func allQuizzes(
+        page: Int,
+        size: Int,
+        completion: @escaping (Result<[PublicQuizItemresponseData], Error>) -> Void
+    ) {
+        let token = KeychainService.shared.read(key: "accessToken") ?? ""
+        
+        let headers: [String: String] = [
+            "Authorization": "Bearer \(token)",
+            "Accept": "application/json"
+        ]
+        
+        let path = SearchEndpoint.allQuizzes(page: page, size: size).path
+        
+        manager.request(
+            endpoint: path,
+            model: PublicQuizResponseModel.self,
+            method: .get,
+            params: nil,
+            encodingType: .url,
+            header: headers,
+            isFullURL: true //  Çünki path tam URL-dir
+        ) { response, error in
+            if let response = response {
+                completion(.success(response.data))
+            } else {
+                let message = error ?? "Naməlum xəta baş verdi"
+                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: message])))
+            }
+        }
+    }
+}
