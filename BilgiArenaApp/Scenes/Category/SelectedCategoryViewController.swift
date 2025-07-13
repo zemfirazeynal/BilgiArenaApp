da925bc852e9dfd516c5495d4f5792f089b836f2
Zemfira Zeynalli <109147342+zemfirazeynal@users.noreply.github.com>
2025-07-14 03:56:14 +0400

    commit
    commit

--- BilgiArenaApp/Scenes/Category/SelectedCategoryViewController.swift
+++ BilgiArenaApp/Scenes/Category/SelectedCategoryViewController.swift
@@ -8,11 +8,15 @@
 import UIKit
 
 final class SelectedCategoryViewController: UIViewController {
-    
-    private let viewModel: SelectedCategoryViewModel
+
     private var selectedIndexPath: IndexPath?
 
-    
+    private lazy var navigationHeader = CustomNavigationHeaderView(
+        title: viewModel.categoryName,
+        showsBackButton: true,
+        titleColor: .white
+    )
+
     private let whiteContainerView: UIView = {
         let view = UIView()
         view.backgroundColor = .white
@@ -24,145 +28,188 @@ final class SelectedCategoryViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    private let tableView: UITableView = {
+    private let quizTableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.backgroundColor = .clear
-        tableView.separatorStyle = .none
         tableView.layer.cornerRadius = 20
-        tableView.showsVerticalScrollIndicator = false
-        tableView.register(
-            QuizTableViewCell.self, forCellReuseIdentifier: "QuizCell")
         return tableView
     }()
-       private lazy var navigationHeader = CustomNavigationHeaderView(
-           title: viewModel.categoryName,
-           showsBackButton: true,
-           titleColor: .white
-       )
 
-       init(viewModel: SelectedCategoryViewModel) {
-           self.viewModel = viewModel
-           super.init(nibName: nil, bundle: nil)
-       }
+    private let viewModel: SelectedCategoryViewModel
 
-       required init?(coder: NSCoder) {
-           fatalError("init(coder:) has not been implemented")
-       }
-//
-       override func viewDidLoad() {
-           super.viewDidLoad()
-//           view.backgroundColor = .app
-           tableView.delegate = self
-           tableView.dataSource = self
-           setupUI()
-           setupActions()
-           bindViewModel() // ⬅️ Bunu əlavə et
-               viewModel.fetchQuizzes() // ⬅️ Və buradan API çağır
-       }
-    
+    init(viewModel: SelectedCategoryViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigationHeader()
+        configureTableView()
+        configureViewAppearance()
+        configureLayout()
+        bindViewModel()
+        viewModel.fetchQuizzes()
+    }
+
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Seçimi sıfırla və görünüşü yenilə
+
         if let selectedIndexPath = selectedIndexPath {
-            tableView.deselectRow(at: selectedIndexPath, animated: true)
+            quizTableView.deselectRow(at: selectedIndexPath, animated: true)
             self.selectedIndexPath = nil
-            tableView.reloadData()
+            quizTableView.reloadData()
         }
     }
 
-       private func setupActions() {
-           navigationHeader.onBackTap = { [weak self] in
-               self?.navigationController?.popViewController(animated: true)
-           }
+    private func configureViewAppearance() {
+        view.backgroundColor = .app
+    }
+
+    private func configureTableView() {
+        quizTableView.delegate = self
+        quizTableView.dataSource = self
+        quizTableView.register(
+            QuizTableViewCell.self,
+            forCellReuseIdentifier: "QuizCell"
+        )
+        quizTableView.separatorStyle = .none
+        quizTableView.separatorInset = .zero
+        quizTableView.layoutMargins = .zero
+        quizTableView.contentInset = .zero
+        quizTableView.preservesSuperviewLayoutMargins = false
+        quizTableView.showsVerticalScrollIndicator = false
+    }
+
+    private func configureNavigationHeader() {
+        navigationHeader.onBackTap = { [weak self] in
+            self?.navigationController?.popViewController(animated: true)
         }
-       private func setupUI() {
-           view.backgroundColor = .app
-//           navigationController?.navigationBar.isHidden = true
+    }
+    private func configureLayout() {
+        [navigationHeader, whiteContainerView, quizTableView].forEach {
+            view.addSubview($0)
+        }
+        whiteContainerView.addSubview(quizTableView)
 
-           view.addSubview(navigationHeader)
-           view.addSubview(whiteContainerView)
-           view.addSubview(tableView)
-
-           whiteContainerView.addSubview(tableView)
-           
-
-           navigationHeader.translatesAutoresizingMaskIntoConstraints = false
-
-           NSLayoutConstraint.activate([
+        NSLayoutConstraint.activate([
             navigationHeader.topAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.topAnchor),
+                equalTo: view.safeAreaLayoutGuide.topAnchor
+            ),
 
             navigationHeader.leadingAnchor.constraint(
-                equalTo: view.leadingAnchor),
+                equalTo: view.leadingAnchor
+            ),
             navigationHeader.trailingAnchor.constraint(
-                equalTo: view.trailingAnchor),
+                equalTo: view.trailingAnchor
+            ),
             navigationHeader.heightAnchor.constraint(equalToConstant: 48),
 
-               whiteContainerView.topAnchor.constraint(
-                   equalTo: navigationHeader.bottomAnchor, constant: 20),
-               whiteContainerView.leadingAnchor.constraint(
-                   equalTo: view.leadingAnchor, constant: 8),
-               whiteContainerView.trailingAnchor.constraint(
-                   equalTo: view.trailingAnchor, constant: -8),
-               whiteContainerView.bottomAnchor.constraint(
-                   equalTo: view.bottomAnchor, constant: -20),
-              
+            whiteContainerView.topAnchor.constraint(
+                equalTo: navigationHeader.bottomAnchor,
+                constant: 20
+            ),
+            whiteContainerView.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: 8
+            ),
+            whiteContainerView.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: -8
+            ),
+            whiteContainerView.bottomAnchor.constraint(
+                equalTo: view.bottomAnchor,
+                constant: -20
+            ),
 
-            tableView.topAnchor.constraint(equalTo: whiteContainerView.topAnchor, constant: 20),
-                tableView.leadingAnchor.constraint(equalTo: whiteContainerView.leadingAnchor),
-                tableView.trailingAnchor.constraint(equalTo: whiteContainerView.trailingAnchor),
-                tableView.bottomAnchor.constraint(equalTo: whiteContainerView.bottomAnchor)
-           ])
-       }
-    
+            quizTableView.topAnchor.constraint(
+                equalTo: whiteContainerView.topAnchor,
+                constant: 20
+            ),
+            quizTableView.leadingAnchor.constraint(
+                equalTo: whiteContainerView.leadingAnchor
+            ),
+            quizTableView.trailingAnchor.constraint(
+                equalTo: whiteContainerView.trailingAnchor
+            ),
+            quizTableView.bottomAnchor.constraint(
+                equalTo: whiteContainerView.bottomAnchor
+            ),
+        ])
+    }
+
     private func bindViewModel() {
         viewModel.onQuizzesLoaded = { [weak self] in
             DispatchQueue.main.async {
-                self?.tableView.reloadData()
+                self?.quizTableView.reloadData()
             }
         }
 
         viewModel.onError = { [weak self] message in
             DispatchQueue.main.async {
-                let alert = UIAlertController(title: "Xəta", message: message, preferredStyle: .alert)
+                let alert = UIAlertController(
+                    title: "Xəta",
+                    message: message,
+                    preferredStyle: .alert
+                )
                 alert.addAction(UIAlertAction(title: "OK", style: .default))
                 self?.present(alert, animated: true)
             }
         }
     }
-   }
+}
 
-   extension SelectedCategoryViewController: UITableViewDelegate, UITableViewDataSource {
-       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return viewModel.numberOfQuizList()
+extension SelectedCategoryViewController: UITableViewDelegate,
+    UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
+        -> Int
+    {
+        return viewModel.numberOfQuizList()
 
-       }
+    }
 
-       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-           
-           guard let cell = tableView.dequeueReusableCell(withIdentifier: "QuizCell", for: indexPath) as? QuizTableViewCell else {
-                   return UITableViewCell()
-               }
-               let quiz = viewModel.quiz(at: indexPath.row)
-               let isSelected = (indexPath == selectedIndexPath)
-               cell.configure(with: quiz, isSelected: isSelected)
-               return cell
-       }
-       
-       func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-           selectedIndexPath = indexPath
-           tableView.reloadData() // Hər şeyi yeniləyir ki, seçilən cell görünüşü dəyişsin
-           viewModel.didSelectItem(at: indexPath.item)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell
+    {
 
-       }
+        guard
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: "QuizCell",
+                for: indexPath
+            ) as? QuizTableViewCell
+        else {
+            return UITableViewCell()
+        }
+        let quiz = viewModel.quiz(at: indexPath.row)
+        let isSelected = (indexPath == selectedIndexPath)
+        cell.configure(with: quiz, isSelected: isSelected)
+        return cell
+    }
 
-       func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-           return 80
-       }
+    func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
+    ) {
+        selectedIndexPath = indexPath
+        tableView.reloadData()
+        viewModel.didSelectItem(at: indexPath.item)
+
+    }
+
+    func tableView(
+        _ tableView: UITableView,
+        heightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
+        return 80
+    }
 }
