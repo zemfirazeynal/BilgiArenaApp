956b89aeff6ef58918acad09279f9adeb096afd4
Zemfira Zeynalli <109147342+zemfirazeynal@users.noreply.github.com>
2025-07-14 23:29:03 +0400

    commit
    commit

--- BilgiArenaApp/Utilities/Profile/ProfileNavigationHeaderView.swift
+++ BilgiArenaApp/Utilities/Profile/ProfileNavigationHeaderView.swift
@@ -9,77 +9,43 @@ import UIKit
 
 class ProfileNavigationHeaderView: UIView {
 
-//       var onBackTap: (() -> Void)?
-       var onSettingsTap: (() -> Void)?
-       
-//       private let backButton: UIButton = {
-//           let button = UIButton(type: .system)
-//           button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
-//           button.tintColor = .white
-//           button.translatesAutoresizingMaskIntoConstraints = false
-//           return button
-//       }()
-       
-       private let settingsButton: UIButton = {
-           let button = UIButton(type: .system)
-           button.setImage(UIImage(systemName: "gearshape"), for: .normal)
-           button.tintColor = .white
-           button.translatesAutoresizingMaskIntoConstraints = false
-           return button
-       }()
-   
-    
-//    init(hideBackButton: Bool = false) {
-//        super.init(frame: .zero)
-//        setupView()
-//
-//        if hideBackButton {
-//            backButton.isHidden = true
-//        }
-//    }
-    
-    
-       
-       override init(frame: CGRect) {
-           super.init(frame: frame)
-           setupView()
-       }
-       
-       required init?(coder: NSCoder) {
-           super.init(coder: coder)
-           setupView()
-       }
-       
-       private func setupView() {
-//           addSubview(backButton)
-           translatesAutoresizingMaskIntoConstraints = false
-           addSubview(settingsButton)
-           
-           NSLayoutConstraint.activate([
-//               backButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-//               backButton.centerYAnchor.constraint(equalTo: centerYAnchor),
-//               backButton.widthAnchor.constraint(equalToConstant: 32),
-//               backButton.heightAnchor.constraint(equalToConstant: 32),
-               
-               heightAnchor.constraint(equalToConstant: 48),
+    var onSettingsTap: (() -> Void)?
 
-            
-               settingsButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-               settingsButton.centerYAnchor.constraint(equalTo: centerYAnchor),
-               settingsButton.widthAnchor.constraint(equalToConstant: 32),
-               settingsButton.heightAnchor.constraint(equalToConstant: 32)
-           ])
-           
-       }
-       
-    
+    private let settingsButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "gearshape"), for: .normal)
+        button.tintColor = .white
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configureLayout() {
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubview(settingsButton)
+
+        NSLayoutConstraint.activate([
+            heightAnchor.constraint(equalToConstant: 48),
+
+            settingsButton.trailingAnchor.constraint(
+                equalTo: trailingAnchor,
+                constant: -16
+            ),
+            settingsButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            settingsButton.widthAnchor.constraint(equalToConstant: 32),
+            settingsButton.heightAnchor.constraint(equalToConstant: 32),
+        ])
+    }
+
     func setSettingsTarget(target: Any?, action: Selector) {
-            settingsButton.addTarget(target, action: action, for: .touchUpInside)
-        }
-
+        settingsButton.addTarget(target, action: action, for: .touchUpInside)
+    }
 }
-
-
-
-
-
