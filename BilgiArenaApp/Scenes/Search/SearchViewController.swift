6947907a3ad8c3238bd19f6f478d167d4679584c
Zemfira Zeynalli <109147342+zemfirazeynal@users.noreply.github.com>
2025-07-14 20:26:41 +0400

    commit
    commit

--- BilgiArenaApp/Scenes/Search/SearchViewController.swift
+++ BilgiArenaApp/Scenes/Search/SearchViewController.swift
@@ -13,11 +13,12 @@ class SearchViewController: UIViewController {
     private let quizList: [Quiz] = Quiz.sampleData
 
     private var selectedIndexPath: IndexPath?
-    
-    private let navigationHeader: CustomNavigationHeaderView = {
-            let header = CustomNavigationHeaderView(title: "Discover", showsBackButton: false, titleColor: .white)
-            return header
-        }()
+
+    private let navigationHeader = CustomNavigationHeaderView(
+        title: "Discover",
+        showsBackButton: false,
+        titleColor: .white
+    )
 
     // MARK: - UI Elements
     private let searchBar: UISearchBar = {
@@ -34,12 +35,10 @@ class SearchViewController: UIViewController {
             attributes: [.foregroundColor: UIColor.white]
         )
         textField.layer.borderWidth = 1
-        textField.layer.borderColor =
-            UIColor(red: 0.8, green: 0.72, blue: 0.7, alpha: 1).cgColor
+        textField.layer.borderColor = UIColor.white.cgColor
         textField.layer.cornerRadius = 12
         textField.layer.masksToBounds = true
         textField.leftView?.tintColor = .white
-
         return sb
     }()
 
@@ -67,7 +66,6 @@ class SearchViewController: UIViewController {
         return label
     }()
 
-
     private let quizHeaderStack: UIStackView = {
         let stack = UIStackView()
         stack.axis = .horizontal
@@ -81,24 +79,129 @@ class SearchViewController: UIViewController {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.backgroundColor = .clear
-        tableView.separatorStyle = .none
         tableView.layer.cornerRadius = 20
-        tableView.showsVerticalScrollIndicator = false
-        tableView.register(
-            QuizTableViewCell.self, forCellReuseIdentifier: "QuizCell")
         return tableView
     }()
 
-     var viewModel: SearchViewModel?
+    var viewModel: SearchViewModel?
+        
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewAppearance()
+        configureTableView()
+        bindViewModel()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
-//    init(viewModel: SearchViewModelProtocol) {
-//        self.viewModel = viewModel
-//        super.init(nibName: nil, bundle: nil)
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
+        if let selectedIndexPath = selectedIndexPath {
+            quizTableView.deselectRow(at: selectedIndexPath, animated: true)
+            self.selectedIndexPath = nil
+            quizTableView.reloadData()
+        }
+    }
+
+    // MARK: - Setup Methods
+    private func configureTableView() {
+        quizTableView.delegate = self
+        quizTableView.dataSource = self
+        quizTableView.register(
+            QuizTableViewCell.self,
+            forCellReuseIdentifier: "QuizCell"
+        )
+        quizTableView.separatorStyle = .none
+        quizTableView.separatorInset = .zero
+        quizTableView.layoutMargins = .zero
+        quizTableView.contentInset = .zero
+        quizTableView.preservesSuperviewLayoutMargins = false
+        quizTableView.showsVerticalScrollIndicator = false
+    }
+
+    private func configureViewAppearance() {
+        view.backgroundColor = .app
+        configureLayout()
+    }
+
+    private func configureLayout() {
+        [navigationHeader, searchBar, containerView].forEach {
+            view.addSubview($0)
+        }
+
+        view.bringSubviewToFront(navigationHeader)
+        quizHeaderStack.addArrangedSubview(quizLabel)
+        containerView.addSubview(quizHeaderStack)
+        containerView.addSubview(quizTableView)
+
+        NSLayoutConstraint.activate([
+            navigationHeader.topAnchor.constraint(
+                equalTo: view.topAnchor,
+                constant: 60
+            ),
+            navigationHeader.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor
+            ),
+            navigationHeader.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor
+            ),
+            navigationHeader.heightAnchor.constraint(equalToConstant: 36),  // MUTLƏQ!
+
+            searchBar.topAnchor.constraint(
+                equalTo: navigationHeader.bottomAnchor,
+                constant: 12
+            ),
+            searchBar.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: 20
+            ),
+            searchBar.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: -20
+            ),
+            searchBar.heightAnchor.constraint(equalToConstant: 44),
+
+            containerView.topAnchor.constraint(
+                equalTo: searchBar.bottomAnchor,
+                constant: 20
+            ),
+            containerView.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: 8
+            ),
+            containerView.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: -8
+            ),
+            containerView.bottomAnchor.constraint(
+                equalTo: view.bottomAnchor,
+                constant: -20
+            ),
+
+            quizLabel.topAnchor.constraint(
+                equalTo: containerView.topAnchor,
+                constant: 16
+            ),
+            quizLabel.leadingAnchor.constraint(
+                equalTo: containerView.leadingAnchor,
+                constant: 16
+            ),
+
+            quizTableView.topAnchor.constraint(
+                equalTo: quizHeaderStack.bottomAnchor,
+                constant: 12
+            ),
+            quizTableView.leadingAnchor.constraint(
+                equalTo: containerView.leadingAnchor
+            ),
+            quizTableView.trailingAnchor.constraint(
+                equalTo: containerView.trailingAnchor
+            ),
+            quizTableView.bottomAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.bottomAnchor
+            ),
+        ])
+    }
 
     private func bindViewModel() {
         viewModel?.onUpdate = { [weak self] in
@@ -107,171 +210,46 @@ class SearchViewController: UIViewController {
             }
         }
         viewModel?.loadAllQuizzes()
-
     }
-    
-   
-
-    //    // MARK: - Lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configureView()
-
-        navigationHeader.translatesAutoresizingMaskIntoConstraints = false
-
-//        navigationHeader.onBackTap = { [weak self] in
-//            print(" Back tapped")
-//            self?.getRootTabBarController()?.selectedIndex = 0
-//        }
-        searchBar.delegate = self
-        bindViewModel()
-        
-    }
-    
-
-    // MARK: - Setup Methods
-    private func configureView() {
-        view.backgroundColor = .app
-
-        quizTableView.delegate = self
-        quizTableView.dataSource = self
-
-        setupHierarchy()
-        setupConstraints()
-
-        //           backButton.addTarget(self, action: #selector(handleBack), for: .touchUpInside)
-    }
-
-    private func setupHierarchy() {
-
-        [navigationHeader, searchBar, containerView].forEach {
-            view.addSubview($0)
-        }
-
-        
-        view.bringSubviewToFront(navigationHeader)
-
-        quizHeaderStack.addArrangedSubview(quizLabel)
-
-        containerView.addSubview(quizHeaderStack)
-        containerView.addSubview(quizTableView)
-    }
-
-    private func setupConstraints() {
-        NSLayoutConstraint.activate([
-
-            navigationHeader.topAnchor.constraint(
-                equalTo: view.topAnchor, constant: 60),
-            navigationHeader.leadingAnchor.constraint(
-                equalTo: view.leadingAnchor),
-            navigationHeader.trailingAnchor.constraint(
-                equalTo: view.trailingAnchor),
-            navigationHeader.heightAnchor.constraint(equalToConstant: 36),  // MUTLƏQ!
-
-        
-            
-            searchBar.topAnchor.constraint(
-                equalTo: navigationHeader.bottomAnchor, constant: 12),
-            searchBar.leadingAnchor.constraint(
-                equalTo: view.leadingAnchor, constant: 20),
-            searchBar.trailingAnchor.constraint(
-                equalTo: view.trailingAnchor, constant: -20),
-            searchBar.heightAnchor.constraint(equalToConstant: 44),
-
-            containerView.topAnchor.constraint(
-                equalTo: searchBar.bottomAnchor, constant: 20),
-            containerView.leadingAnchor.constraint(
-                equalTo: view.leadingAnchor, constant: 8),
-            containerView.trailingAnchor.constraint(
-                equalTo: view.trailingAnchor, constant: -8),
-            containerView.bottomAnchor.constraint(
-                equalTo: view.bottomAnchor, constant: -20),
-
-            quizLabel.topAnchor.constraint(
-                equalTo: containerView.topAnchor, constant: 16),
-            quizLabel.leadingAnchor.constraint(
-                equalTo: containerView.leadingAnchor, constant: 16),
-
-
-            quizTableView.topAnchor.constraint(
-                equalTo: quizHeaderStack.bottomAnchor, constant: 12),
-            quizTableView.leadingAnchor.constraint(
-                equalTo: containerView.leadingAnchor),
-            quizTableView.trailingAnchor.constraint(
-                equalTo: containerView.trailingAnchor),
-            quizTableView.bottomAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-
-        ])
-    }
-
-//    // MARK: - Actions
-//    @objc private func handleBack() {
-//        getRootTabBarController()?.selectedIndex = 0  // 0: Home
-//
-//    }
-//
-//    private func getRootTabBarController() -> UITabBarController? {
-//        UIApplication.shared.connectedScenes
-//            .compactMap { $0 as? UIWindowScene }
-//            .first?
-//            .windows
-//            .first?
-//            .rootViewController as? UITabBarController
-//    }
 }
 
 // MARK: - UITableViewDelegate & UITableViewDataSource
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
-//        -> Int
-//    {
-//        return viewModel?.numberOfQuizzes() ?? 0
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
-//        -> UITableViewCell {
-//        let cell =
-//            tableView.dequeueReusableCell(
-//                withIdentifier: "QuizCell", for: indexPath)
-//            as! QuizTableViewCell
-//            if let quiz = viewModel?.quiz(at: indexPath.row) {
-//                let isSelected = (indexPath == selectedIndexPath)
-//                cell.configure(with: quiz, isSelected: isSelected)
-//            }
-//        return cell
-//    }
-//
-//    
-//   
-//
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return viewModel?.numberOfQuizzes() ?? 0
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
+        -> Int
+    {
+        return viewModel?.numberOfQuizzes() ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell
+    {
+        guard
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: "QuizCell",
+                for: indexPath
+            ) as? QuizTableViewCell,
+            let quiz = viewModel?.quiz(at: indexPath.row)
+        else {
+            return UITableViewCell()
         }
 
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            guard
-                let cell = tableView.dequeueReusableCell(withIdentifier: "QuizCell", for: indexPath) as? QuizTableViewCell,
-                let quiz = viewModel?.quiz(at: indexPath.row)
-            else {
-                return UITableViewCell()
-            }
+        let isSelected = indexPath == selectedIndexPath
+        cell.configure(with: quiz, isSelected: isSelected)
+        return cell
+    }
 
-            let isSelected = indexPath == selectedIndexPath
-            cell.configure(with: quiz, isSelected: isSelected)
-            return cell
-        }
-    
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
+    ) {
         selectedIndexPath = indexPath
-            tableView.reloadData()
-        viewModel?.didSelectItem(at: indexPath.row) 
-
+        tableView.reloadData()
+        viewModel?.didSelectItem(at: indexPath.row)
     }
     func tableView(
-        _ tableView: UITableView, heightForRowAt indexPath: IndexPath
+        _ tableView: UITableView,
+        heightForRowAt indexPath: IndexPath
     ) -> CGFloat {
         return 80
     }
@@ -280,6 +258,6 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
 
 extension SearchViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-           viewModel?.filterQuizzes(with: searchText)
-       }
+        viewModel?.filterQuizzes(with: searchText)
+    }
 }
